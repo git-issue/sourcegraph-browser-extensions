e99a467acbc6deeeb446a236a6e2f61514338f1f
Beyang Liu <beyang.liu@gmail.com>
2014-01-22 23:52:36 -0800

    chrome extension annotate GitHub files

--- chrome/github.js
+++ chrome/github.js
@@ -1,7 +1,7 @@
 function main() {
   var page = new GitHubPage(window.location.href, document);
-  if (page.valid) {
-    console.log('Valid GitHub page:', page.repo);
+  if (page.isValid()) {
+    console.log('Valid GitHub page:', page.info);
     page.inject();
   } else {
     console.log('Not a valid GitHub page:', window.location.href);
@@ -22,29 +22,48 @@ window.setInterval(function() {
 }, 1000);
 
 function GitHubPage(url, doc) {
+  this.isValid = function() {
+    return this.isRepoPage || this.isFilePage;
+  };
+
   // Ensure it's a public repo.
   if (!doc.querySelector('body.vis-public')) return;
 
   this.url = url;
-  this.repo = parseURL(this.url);
-  if (!this.repo) return;
+  this.info = parseURL(this.url);
+  if (!this.info) return;
 
   this.doc = doc;
-  this.precedesElem = doc.querySelector('div.file-navigation.in-mid-page');
-  if (!this.precedesElem) return;
+  this.postPanelElem = doc.querySelector('div.file-navigation.in-mid-page');
+  if (this.postPanelElem) {
+    this.isRepoPage = true;
+  }
 
-  this.valid = true;
+  this.codeElem = doc.querySelector('table.file-code');
+  if (this.codeElem) {
+    this.isFilePage = true;
+  }
 
   this.inject = function() {
     if (doc.__sg_injected) return;
     doc.__sg_injected = true;
+
+    if (this.isRepoPage) {
+      this.injectPanel();
+    }
+    if (this.isFilePage) {
+      this.injectAnnotations();
+    }
+  };
+
+  this.injectPanel = function() {
     var div = document.createElement('div');
     div.id = 'sg-container';
     var iframe = document.createElement('iframe');
     iframe.id = 'sg';
-    iframe.src = '<%= url %>/_ext/chrome-ext/repos/' + this.repo.id;
+    iframe.src = '<%= url %>/_ext/chrome-ext/repos/' + this.info.repoid;
     div.appendChild(iframe);
-    this.precedesElem.insertBefore(div);
+    this.postPanelElem.insertBefore(div);
 
     window.addEventListener('message', function(e) {
       var height = e.data.height;
@@ -55,14 +74,39 @@ function GitHubPage(url, doc) {
     }, false);
   };
 
+  this.injectAnnotations = function() {
+    var codeContainer = this.codeElem.querySelector('pre');
+    var codeHTML = codeContainer.innerHTML;
+    var reqBody = JSON.stringify({
+      'lang': 'python',
+      'repos': ['github.com/mitsuhiko/flask'],
+
+      'file': this.info.path,
+      'repo': this.info.repoid,
+      'snippets': [codeHTML],
+    });
+
+    var req = new XMLHttpRequest();
+    req.onload = function() {
+      // TODO: too slow
+      codeContainer.innerHTML = this.response.snippets[0];
+    };
+    req.open('post', 'http://localhost:3000/api/snippet', true);
+    req.responseType = 'json';
+    req.send(reqBody);
+  };
+
   function parseURL(url) {
-    var m = url.match(/^https:\/\/github\.com\/([^/]+)\/([^/]+)/);
+    var m = url.match(/^https:\/\/github\.com\/([^\/]+)\/([^\/]+)(?:\/blob\/([^\/]+)\/(.+))?/);
+
     if (m) {
-      var owner = m[1], name = m[2];
+      var owner = m[1], name = m[2], branch = m[3], path = m[4];
       return {
-        id: 'github.com/' + owner + '/' + name,
+        repoid: 'github.com/' + owner + '/' + name,
         owner: owner,
         name: name,
+        branch: branch,
+        path: path,
       };
     }
   }
